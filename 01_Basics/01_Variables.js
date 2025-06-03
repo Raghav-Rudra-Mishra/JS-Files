fbef88846f338eceaf9e5e6f38c2e1d1d58c6ca4
Raghav-Rudra-Mishra <149260369+Raghav-Rudra-Mishra@users.noreply.github.com>
2025-06-03 06:35:23 +0000

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553        //can't be changed later
+let accountEmail = "raghav@gmail.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2        nah not possible
+accountEmail = "rm@gmail.com"
+accountPass = "98765"
+accountCity = "Lucknow"
+
+console.log(accountId)
+console.table([accountEmail,accountId,accountPass,accountCity])
