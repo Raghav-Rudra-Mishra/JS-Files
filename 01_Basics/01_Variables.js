256852ce90ad5ee8c25db2b2a191754da72689a7
Raghav-Rudra-Mishra <149260369+Raghav-Rudra-Mishra@users.noreply.github.com>
2025-06-03 06:35:38 +0000

    variables

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -10,3 +10,12 @@ accountCity = "Lucknow"
 
 console.log(accountId)
 console.table([accountEmail,accountId,accountPass,accountCity])
+/*
+Const use to declare constant values
+var is not used now because it does not know or define any scope of variable and has issue in block scope and functional scope, but 
+Let has its scope and so it is only preferred to use now. Var is depreciated.
+
+Variable can also be declared without any let and bar but it is very bad practice
+
+If any variable is declared and not initialized, JavaScript considers it “undefined” type and printing that variable would give ‘undefined’ to you.
+*/ 
