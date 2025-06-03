69f9312b0b778a50f4a7e14bb8c1f4cdcb003411
Raghav-Rudra-Mishra <149260369+Raghav-Rudra-Mishra@users.noreply.github.com>
2025-06-03 08:06:54 +0000

    more

---
+++ 01_Basics/comparisonsJS.js
@@ -0,0 +1,19 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2<=1)
+console.log(2!=1)
+console.log(2==1)
+
+console.log(null>0) // => false
+console.log(null==0) // => false
+console.log(null>=0) // => true
+console.log(null===0) // => false , === is for strict checking
+
+//strict checking is something where datatypes are also checked as "5"==5 is true but "5"===5 is false
+
+//all beolw falses
+console.log(undefined>0)
+console.log(undefined==0)
+console.log(undefined>=0)
+console.log(undefined===0)
---
+++ 01_Basics/someMore.js
@@ -0,0 +1,26 @@
+let value = 3
+let negValue = -value
+
+console.log(2+3)
+console.log(2-3)
+console.log(2*3)
+console.log(2/3)
+console.log(2**3)
+console.log(2%3)
+
+let str1 = "hello "
+let str2 = "raghav"
+let str3 = str1+str2
+console.log(str3)   //strings can be eadily added like this
+
+// all these strings below print 12
+console.log("1"+2) //->12
+console.log(1+"2") //->12
+console.log("1"+"2") //->12
+console.log("1"+2+2) //->122
+console.log(1+2+"2") //->32 (why not 122 again?)
+
+/* 
+Rule:
+whatever you have taken as first variable, every successive nrighbour works similarly.
+*/
